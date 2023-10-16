3805c5831ef4d114a2f05ea9b239faddde2508dc
JuanJavierRuiz <123001800+JuanJavierRuiz@users.noreply.github.com>
2023-10-15 23:51:12 -0400

    7.18 Done

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -22,12 +22,22 @@ public class LogAnalyzer
         // Create the reader to obtain the data.
         reader = new LogfileReader("demo.log");
     }
+    
+    public LogAnalyzer(String logFile)
+    { 
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader(logFile);
+    }
 
     /**
      * Analyze the hourly access data from the log file.
      */
     public void analyzeHourlyData()
     {
+        reader.reset();
         while(reader.hasNext()) {
             LogEntry entry = reader.next();
             int hour = entry.getHour();
@@ -54,5 +64,60 @@ public class LogAnalyzer
     public void printData()
     {
         reader.printData();
+    }
+    
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        for (int position = 0; position<24; position++)
+        {
+            total = total + hourCounts[position];
+        }
+        return total;
+    }
+    
+    public int busiestHour()
+    {
+        int busyHour = 0;
+        for (int position = 1; position<24; position++)
+        {
+            if (hourCounts[position] > hourCounts[busyHour])
+            {
+                busyHour = position;
+            }
+        }
+        return busyHour;
+    }
+    
+    public int quietestHour()
+    {
+        int quietHour = 0;
+        for (int position = 1; position<24; position++)
+        {
+            if (hourCounts[position] < hourCounts[quietHour])
+            {
+                quietHour = position;
+            }
+        }
+        return quietHour;
+    }
+    
+    public int busiestTwoHour()
+    {
+        int busyTwoHour = 0;
+        int totalbusyTwoHour = hourCounts[0] + hourCounts[1];
+        for (int position = 1; position<23; position++)
+        {
+            if (hourCounts[position] + hourCounts [position + 1] > totalbusyTwoHour)
+            {
+                busyTwoHour = position;
+                totalbusyTwoHour = hourCounts[position] + hourCounts [position + 1];
+            }
+        }
+        if (hourCounts[23] + hourCounts[0] > totalbusyTwoHour)
+        {
+            busyTwoHour = 23;
+        }
+        return busyTwoHour;
     }
 }
